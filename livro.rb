f8b1f94758be391b0269b6bae2ab59b2eb19c1f6
lcamilo <lcamilo@esec.com.br>
2016-09-19 23:21:29 -0300

    master: corrigindo metodo to_csv()

--- livro.rb
+++ livro.rb
@@ -1,28 +1,73 @@
 class Livro
-	attr_reader :titulo, :preco, :ano_lancamento
-	def initialize(titulo, preco, ano_lancamento)
+	attr_reader :titulo, :preco, :ano_lancamento, :possui_reimpressao
+	def initialize(titulo, preco, ano_lancamento, possui_reimpressao)
 		@titulo = titulo
 		@preco = preco
 		@ano_lancamento = ano_lancamento
+		@possui_reimpressao = possui_reimpressao
 		@preco = calcula_preco(preco)
 	end
+	private
 	def calcula_preco(base) 
-		if @ano_lancamento < 2000
-			base * 0.7
+		if @ano_lancamento < 2006
+			if @possui_reimpressao 
+				 base * 0.9
+			else
+				 base * 0.95
+			end
+		elsif @ano_lancamento <= 2010
+			base * 0.96
 		else
-			base
+			base 
 		end
 	end
-end
-livro_rails = Livro.new("Agile develpment with rails", 70, 2011)
-livro_ruby = Livro.new("Programming Ruby 1.9", 60,2010)
-
-def imprime_nota_fiscal(livros)
-	livros.each do | livro |
-		puts "Titulo #{livro.titulo} - #{livro.preco}"
+	public
+	def possui_reimpressao?
+		@possui_reimpressao
+	end
+	public
+	def to_csv
+		"#{@titulo},#{@preco}, #{@ano_lancamento}"
 	end
 end
 
-livros = [livro_ruby,livro_rails]
-imprime_nota_fiscal(livros)
 
+class Estoque
+	def initialize
+		@livros = [];
+	end
+	def imprime_nota_fiscal()
+		@livros.each do | livro |
+			puts " "
+			puts "Titulo #{livro.titulo}"
+			puts "Preco #{livro.preco}"
+			puts "Ano Lancamento #{livro.ano_lancamento}"
+			puts "Possui impressao #{livro.possui_reimpressao}"
+		end
+	end
+	def exporta_csv()
+		@livros.each do | livro |
+			puts livro.to_csv
+		end
+	end
+	def mais_barato_que(estoque, valor)
+		@livros.select do | livro |
+			livro.preco <= valor
+		end
+	end
+	def total_livro 
+		@livros.size
+	end
+	def adiciona(livro)
+		@livros << livro if livro
+	end
+end
+
+estoque = Estoque.new
+estoque.adiciona Livro.new("Agile develpment with rails", 70, 2011, true)
+estoque.adiciona Livro.new("Programming Ruby 1.9", 60, 2010, true)
+
+estoque.adiciona Livro.new("The pragmatic Programming", 70, 1999, true)
+estoque.adiciona Livro.new("Programming Ruby", 100, 2004, true)
+
+estoque.exporta_csv()
